cb04f09679bb55896b554cdd1dee69ef8ebfa378
Alberto Gregoris <alberto.gregoris@intelygenz.com>
2014-09-09 17:56:15 +0200

    new MockHydraClient

--- client/mock/mock_hydra_client.go
+++ client/mock/mock_hydra_client.go
@@ -5,6 +5,9 @@ package mock_client
 
 import (
 	gomock "code.google.com/p/gomock/gomock"
+	time "time"
+
+	. "github.com/innotech/hydra-go-client/client"
 )
 
 // Mock of HydraClient interface
@@ -28,12 +31,15 @@ func (_m *MockHydraClient) EXPECT() *_MockHydraClientRecorder {
 	return _m.recorder
 }
 
-func (_m *MockHydraClient) Get() {
-	_m.ctrl.Call(_m, "Get")
+func (_m *MockHydraClient) Get(appId string, forceCacheRefresh bool) ([]string, error) {
+	ret := _m.ctrl.Call(_m, "Get", appId, forceCacheRefresh)
+	ret0, _ := ret[0].([]string)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
 }
 
-func (_mr *_MockHydraClientRecorder) Get() *gomock.Call {
-	return _mr.mock.ctrl.RecordCall(_mr.mock, "Get")
+func (_mr *_MockHydraClientRecorder) Get(arg0, arg1 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCall(_mr.mock, "Get", arg0, arg1)
 }
 
 func (_m *MockHydraClient) ReloadAppServers() {
@@ -50,4 +56,36 @@ func (_m *MockHydraClient) ReloadHydraServers() {
 
 func (_mr *_MockHydraClientRecorder) ReloadHydraServers() *gomock.Call {
 	return _mr.mock.ctrl.RecordCall(_mr.mock, "ReloadHydraServers")
+}
+
+func (_m *MockHydraClient) SetAppsCacheMonitor(monitor *AppsCacheMonitor) {
+	_m.ctrl.Call(_m, "SetAppsCacheMonitor", monitor)
+}
+
+func (_mr *_MockHydraClientRecorder) SetAppsCacheMonitor(arg0 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCall(_mr.mock, "SetAppsCacheMonitor", arg0)
+}
+
+func (_m *MockHydraClient) SetHydraCacheMonitor(monitor *HydraCacheMonitor) {
+	_m.ctrl.Call(_m, "SetHydraCacheMonitor", monitor)
+}
+
+func (_mr *_MockHydraClientRecorder) SetHydraCacheMonitor(arg0 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCall(_mr.mock, "SetHydraCacheMonitor", arg0)
+}
+
+func (_m *MockHydraClient) SetMaxNumberOfRetriesPerHydraServer(numberOfRetries uint) {
+	_m.ctrl.Call(_m, "SetMaxNumberOfRetriesPerHydraServer", numberOfRetries)
+}
+
+func (_mr *_MockHydraClientRecorder) SetMaxNumberOfRetriesPerHydraServer(arg0 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCall(_mr.mock, "SetMaxNumberOfRetriesPerHydraServer", arg0)
+}
+
+func (_m *MockHydraClient) SetWaitBetweenAllServersRetry(duration time.Duration) {
+	_m.ctrl.Call(_m, "SetWaitBetweenAllServersRetry", duration)
+}
+
+func (_mr *_MockHydraClientRecorder) SetWaitBetweenAllServersRetry(arg0 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCall(_mr.mock, "SetWaitBetweenAllServersRetry", arg0)
 }
