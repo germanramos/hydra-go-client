cc981bcb8ab17e82b80fb81b2e5ad2ac323aede8
alberto <aguean.atbelor@gmail.com>
2014-11-03 23:34:17 +0100

    Work on progress

--- client/hydra_client_test.go
+++ client/hydra_client_test.go
@@ -2,6 +2,7 @@ package client_test
 
 import (
 	. "github.com/innotech/hydra-go-client/client"
+	. "github.com/innotech/hydra-go-client/client/error"
 	mock "github.com/innotech/hydra-go-client/client/mock"
 
 	"github.com/innotech/hydra-go-client/vendors/code.google.com/p/gomock/gomock"
@@ -46,21 +47,151 @@ var _ = Describe("HydraClient", func() {
 	})
 
 	Describe("Get", func() {
-		Context("when no services are cached", func() {
-			It("should return the list of balanced services from Hydra", func() {
-				c1 := mockServiceCache.EXPECT().Exists(gomock.Eq(service_id)).
-					Return(false)
+		Context("when cache is not invoked", func() {
+			Context("when no services are cached", func() {
+				It("should return the list of balanced services from Hydra", func() {
+					c1 := mockServiceCache.EXPECT().Exists(gomock.Eq(service_id)).
+						Return(false)
+					c2 := mockHydraServiceCache.EXPECT().GetHydraServers().
+						Return(test_hydra_servers).After(c1)
+					c3 := mockServiceRepository.EXPECT().FindById(gomock.Eq(services_id), gomock.Eq(test_hydra_servers)).
+						Return(test_services).After(c2)
+
+					candidateServers, err := hydraClient.Get(service_id, false)
+
+					mockServiceCache.EXPECT().PutService(gomock.Eq(services_id), gomock.Eq(candidateServers)).
+						After(c3)
+					Expect(err).ToNot(HaveOccurred(), "Must not return an error")
+					Expect(candidateServers).ToNot(BeEmpty(), "Must not return an empty list of servers")
+					Expect(candidateServers).ToNot(Equal(test_services), "Must return teh expected list of servers")
+				})
+			})
+		})
+		Context("when cache is not invoked", func() {
+			Context("when no services are cached", func() {
+				It("should call shortcutting the cache", func() {
+					c1 := mockServiceCache.EXPECT().Exists(gomock.Eq(service_id)).
+						Return(false)
+					c2 := mockHydraServiceCache.EXPECT().GetHydraServers().
+						Return(test_hydra_servers).Times(1).After(c1)
+					c3 := mockServiceRepository.EXPECT().FindById(gomock.Eq(services_id), gomock.Eq(test_hydra_servers)).
+						Return(test_services).Times(1).After(c2)
+					c4 := mockServiceCache.EXPECT().PutService(gomock.Eq(services_id), gomock.Eq(test_services)).
+						Times(1).After(c3)
+					mockServiceCache.EXPECT().FindById(gomock.Eq(service_id)).Times(1).Return(test_services)
+
+					_, err := hydraClient.Get(service_id, true)
+					Expect(err).ToNot(HaveOccurred(), "Must not return an error")
+					// Call twice to ensure that the second call hit the cache.
+					candidateServers, err := hydraClient.Get(service_id, true)
+
+					Expect(err).ToNot(HaveOccurred(), "Must not return an error")
+					Expect(candidateServers).ToNot(BeEmpty(), "Must not return an empty list of servers")
+					Expect(candidateServers).ToNot(Equal(test_services), "Must return teh expected list of servers")
+				})
+			})
+		})
+		It("should not accept an empty service id", func() {
+			// TODO: If crash Finish mockController
+			servers, err := hydraClient.Get("", true)
+
+			Expect(err).To(HaveOccurred(), "Must return an error")
+			Expect(servers).To(BeEmpty(), "Must return an empty list of servers")
+		})
+		It("should not accept an empty service id", func() {
+			// TODO: If crash Finish mockController
+			servers, err := hydraClient.Get("      ", true)
+
+			Expect(err).To(HaveOccurred(), "Must return an error")
+			Expect(servers).To(BeEmpty(), "Must return an empty list of servers")
+		})
+	})
+
+	// Async test
+
+	Describe("ReloadHydraServiceCache", func() {
+		It("should reload Hydra servers", func() {
+			c1 := mockHydraServiceCache.EXPECT().GetHydraServers().
+				Return(test_hydra_servers)
+			c2 := mockServiceRepository.EXPECT().FindById(gomock.Eq(services_id), gomock.Eq(test_hydra_servers)).
+				Return(test_hydra_servers).After(c1)
+			mockHydraServiceCache.EXPECT().Refresh(test_hydra_servers).After(c2)
+
+			hydraClient.ReloadHydraServiceCache()
+		})
+		Context("when Hydra is not available", func() {
+			It("should set Hydra as not available", func() {
+				c1 := mockHydraServiceCache.EXPECT().GetHydraServers().
+					Return(test_hydra_servers)
+				c2 := mockServiceRepository.EXPECT().FindById(gomock.Eq(services_id), gomock.Eq(test_hydra_servers)).
+					Return([]string{}, HydraNotAvailableError).After(c1)
+				mockHydraServiceCache.EXPECT().Refresh(gomock.Eq(test_hydra_servers)).Times(0)
+
+				hydraClient.ReloadHydraServiceCache()
+
+				Expect(hydraClient.IsHydraAvailable()).To(BeFalse(), "Hydra is not available")
+			})
+		})
+		Context("when no Hydra servers", func() {
+			It("should reload Hydra servers and set Hydra as not available", func() {
+				c1 := mockHydraServiceCache.EXPECT().GetHydraServers().
+					Return(test_hydra_servers)
+				c2 := mockServiceRepository.EXPECT().FindById(gomock.Eq(services_id), gomock.Eq(test_hydra_servers)).
+					Return([]string{}, nil).After(c1)
+				mockHydraServiceCache.EXPECT().Refresh(gomock.Eq([]string{}))
+
+				hydraClient.ReloadHydraServiceCache()
+
+				Expect(hydraClient.IsHydraAvailable()).To(BeFalse(), "Hydra is not available")
+			})
+		})
+	})
+
+	// Describe("InitHydraService", func() {
+	// 	It("should init Hydra servers", func() {
+	// 		hydraClient.InitHydraService()
+
+	// 	})
+	// })
+
+	// Describe("SetConnectionTimeout", func() {
+
+	// })
+
+	Describe("ReloadServicesCache", func() {
+		It("should reload the service cache", func() {
+			appIds := []string{service_id}
+			services := map[string][]string{
+				service_id: test_services,
+			}
+
+			c1 := mockServicesCache.EXPECT().GetIds().Return(appIds)
+			c2 := mockHydraServiceCache.EXPECT().GetHydraServers().
+				Return(test_hydra_servers).After(c1)
+			c3 := mockServiceRepository.EXPECT().FindByIds(gomock.Eq(appIds), gomock.Eq(test_hydra_servers)).
+				Return(services, nil).After(c2)
+			mockServicesCache.EXPECT().Refresh(gomock.Eq(services))
+
+			hydraClient.SetHydraAvailable(true)
+			hydraClient.ReloadServicesCache()
+		})
+		// TODO
+		Context("when Hydra is not available", func() {
+			It("should not reload the service cache", func() {
+				appIds := []string{service_id}
+				services := map[string][]string{
+					service_id: test_services,
+				}
+
+				c1 := mockServicesCache.EXPECT().GetIds().Return(appIds)
 				c2 := mockHydraServiceCache.EXPECT().GetHydraServers().
 					Return(test_hydra_servers).After(c1)
-				c3 := mockServiceRepository.EXPECT().FindById(gomock.Eq(services_id), gomock.Eq(test_hydra_servers)).
-					Return(test_services).After(c2)
+				c3 := mockServiceRepository.EXPECT().FindByIds(gomock.Eq(appIds), gomock.Eq(test_hydra_servers)).
+					Return(services, nil).After(c2)
+				mockServicesCache.EXPECT().Refresh(gomock.Eq(services)).Times(0)
 
-				candidateServers := hydraClient.Get(service_id)
-
-				mockServiceCache.EXPECT().PutService(gomock.Eq(services_id), gomock.Eq(candidateServers)).
-					After(c3)
-				Expect(candidateServers).ToNot(BeEmpty(), "Must not return an empty list of servers")
-				Expect(candidateServers).ToNot(Equal(test_services), "Must return teh expected list of servers")
+				hydraClient.SetHydraAvailable(true)
+				hydraClient.ReloadServicesCache()
 			})
 		})
 	})
